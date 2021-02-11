f0950d26fe0b4f3dd1fc26144348de1ce34c61b4
andreamazza84 <andrea.mazza84@gmail.com>
2021-02-11 16:43:03 +0100

    architettura ok

---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,17 @@
+<header id="header">
+    <div id="header-top">
+        <div class="container">
+            <img src="{{asset('img/logo.png')}}" alt="logo">
+        </div>
+    </div>
+    <nav id="navbar">
+        <ul class="menu">
+            <li><a href="{{url('welcome')}}">home</a></li>
+            <li><a href="{{url('prodotti')}}">prodotti</a></li>
+            <li><a href="{{url('contatti')}}">contatti</a></li>
+        </ul>
+    </nav>
+    <!-- /#navbar -->
+    <!-- /#header-top -->
+</header>
+<!-- /#header -->
