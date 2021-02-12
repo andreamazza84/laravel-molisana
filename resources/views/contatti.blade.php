2bb877858d2b956822de791b6c6a8b92f2c350d3
andreamazza84 <andrea.mazza84@gmail.com>
2021-02-12 16:52:53 +0100

    layout ok

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -6,7 +6,17 @@
 
 @section('contacts')
     <div id="contacts" class="container">
-
+        <h1>Contatti</h1>
+        <form action="#">
+            <label for="name">Nome:</label>
+            <input type="text" name="name" id="name">
+            <label for="surname">Cognome:</label>
+            <input type="text" name="surname" id="surname">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="contact-us">Scrivi qui il tuo messaggio</label>
+            <textarea name="contact-us" id="contact-us" cols="30" rows="10"></textarea>
+            <input type="submit" value="Invia" class="btn">
+        </form>
     </div>
-    <h1>Contatti</h1>
 @endsection
